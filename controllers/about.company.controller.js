467026e3f436500b11ce78a82710ca9d05eec45e
GeoS74 <gsirotkin@yandex.ru>
2023-02-16 16:36:15 +0500

    fixed about controller

--- controllers/about.company.controller.js
+++ controllers/about.company.controller.js
@@ -2,7 +2,7 @@ const About = require('../models/About');
 const mapper = require('../mappers/about.mapper');
 
 module.exports.get = async (ctx) => {
-  const about = await About.findOne({ alias: ctx.params.alias });
+  const about = _getAbout(ctx.params.alias);
   if (!about) {
     ctx.throw(404, 'alias not found');
   }
@@ -11,30 +11,20 @@ module.exports.get = async (ctx) => {
 };
 
 module.exports.getAll = async (ctx) => {
-  const abouts = await About.find().sort({ _id: 1 });
+  const abouts = await _getAboutAll();
 
   ctx.status = 200;
   ctx.body = abouts.map((about) => (mapper(about)));
 };
 
 module.exports.add = async (ctx) => {
-  const about = await About.create({
-    mdInfo: ctx.request.body.mdInfo,
-    alias: ctx.request.body.alias,
-  });
+  const about = await _addAbout(ctx.request.body.alias, ctx.request.body.mdInfo);
   ctx.status = 201;
   ctx.body = mapper(about);
 };
 
 module.exports.update = async (ctx) => {
-  const about = await About.findOneAndUpdate(
-    { alias: ctx.params.alias },
-    { mdInfo: ctx.request.body.mdInfo.trim() },
-    {
-      new: true,
-      runValidators: true, // запускает валидаторы схемы перед записью
-    },
-  );
+  const about = await _updateAbout(ctx.request.body.alias, ctx.request.body.mdInfo);
 
   if (!about) {
     ctx.throw(404, 'alias page not found');
@@ -44,14 +34,38 @@ module.exports.update = async (ctx) => {
 };
 
 module.exports.delete = async (ctx) => {
-  const about = await About.findOneAndDelete(
-    { alias: ctx.params.alias },
-  );
+  const about = await _deleteAbout(ctx.params.alias);
 
   if (!about) {
     ctx.throw(404, 'alias page not found');
   }
-
   ctx.status = 200;
   ctx.body = mapper(about);
 };
+
+function _getAbout(alias) {
+  return About.findOne({ alias });
+}
+
+function _getAboutAll() {
+  return About.find().sort({ _id: 1 });
+}
+
+function _addAbout(alias, mdInfo) {
+  return About.create({ alias, mdInfo });
+}
+
+function _updateAbout(alias, mdInfo) {
+  return About.findOneAndUpdate(
+    { alias },
+    { mdInfo },
+    {
+      new: true,
+      runValidators: true, // запускает валидаторы схемы перед записью
+    },
+  );
+}
+
+function _deleteAbout(alias) {
+  return About.findOneAndDelete({ alias });
+}
