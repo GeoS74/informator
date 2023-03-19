ec6e7db5d2ca62b98850a1a1b924bac989f009fa
GeoS74 <gsirotkin@yandex.ru>
2023-03-19 17:44:20 +0300

    ...

--- controllers/access.setting.controller.js
+++ controllers/access.setting.controller.js
@@ -9,7 +9,7 @@ module.exports.get = async (ctx) => {
 };
 
 module.exports.add = async (ctx) => {
-  await _addRoleToTasks(ctx.request.body);
+  await _saveAccessSettings(ctx.request.body);
 
   ctx.status = 200;
   ctx.body = {
@@ -24,27 +24,113 @@ async function _getAccessSettings() {
     .populate('directings.tasks.actions');
 }
 
-async function _addRoleToTasks(roles) {
+async function _saveAccessSettings(roles) {
+  // console.log('~~~~~~roles~~~~~~~~')
   // console.log(roles)
+
   for (const role in roles) {
     if ({}.hasOwnProperty.call(roles, role)) {
-      const tasks = _getIdActiveTasks(roles[role]);
-      // console.log(tasks)
+      const directings = _getDirectings(roles[role]);
+
+      console.log('~~~~~~~directings~~~~~~~')
+      // console.log(directings)
+      directings.map(e => {
+        console.log(e)
+
+        console.log('----------actions-------------')
+        e.tasks.map(t => {
+          console.log(t)
+        })
+      })
+
 
       await Role.findByIdAndUpdate(
         role.substring(3),
-        { tasks },
+        { directings },
       );
     }
   }
 }
 
-function _getIdActiveTasks(role) {
+/*
+в зависимости от того сколько чекбоксов на клиенте включено, 
+сервер может получать следующие структуры:
+
+~~~~~~направление активировано, задачи - нет~~~~~~~~
+{
+  id_role: { id_directing: [ 'on' ] }
+}
+~~~~~~направление активировано, задачи - тоже~~~~~~~~
+{
+  id_role: {
+    id_directing: { '0': 'on', id_task: [Array] }
+  }
+}
+*/
+
+
+function _getDirectings(role) {
   const result = [];
+
+  console.log('~~~~~~~~role~~~~~~~~')
   console.log(role)
 
+
+  for (const idDirecting in role) {
+
+    // console.log(role[idDirecting])
+
+    // if (role[idDirecting]['0']) {
+    if (Object.keys(role[idDirecting]).length > 1) {
+
+      result.push({
+        directing: idDirecting.substring(3),
+
+        tasks: _getTasks(role[idDirecting])
+      })
+    }
+  }
+  return result;
+}
+
+function _getTasks(directing) {
+   console.log('~~~directing~~~~~~')
+  console.log(directing)
+ 
+
+  const result = [];
+  for (const idTask in directing) {
+
+
+    
+
+    if (idTask !== '0') {
+      console.log(idTask);
+
+      result.push({
+        task: idTask.substring(3),
+
+        // actions: []
+
+        actions: Object.keys(directing[idTask])
+          .filter((a) => (a !== '0'))
+          .map((a) => a.substring(3)),
+      })
+    }
+  }
+  return result;
+}
+
+
+function ___getStructure(role) {
+  const result = [];
+  // console.log(role)
+  // console.log('~~~~~~~~~~~~~~~~~~~~~')
+
   for (const idTask in role) {
+
     if (role[idTask]['0']) {
+
       result.push({
         task: idTask.substring(3),
 
--- mappers/access.setting.mapper.js
+++ mappers/access.setting.mapper.js
@@ -2,17 +2,17 @@ module.exports = (data) => data.map((role) => ({
   id: role._id,
   title: role.title,
 
-  directings: role.directings.map(d => ({
-    id: d.directing._id,
-    title: t.task.title,
+  // directings: role.directings.map(d => ({
+  //   id: d.directing._id,
+  //   title: t.task.title,
 
-    tasks: role.tasks.map((t) => ({
-      id: t.task._id,
-      title: t.task.title,
-      actions: t.actions.map((action) => ({
-        id: action._id,
-        title: action.title,
-      })),
-    })),
-  }))
+  //   tasks: role.tasks.map((t) => ({
+  //     id: t.task._id,
+  //     title: t.task.title,
+  //     actions: t.actions.map((action) => ({
+  //       id: action._id,
+  //       title: action.title,
+  //     })),
+  //   })),
+  // }))
 }));
