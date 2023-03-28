67db3ddeeb63c53cda4b1960f4d2bc3716c2ee0f
geos <gsirotkin@yandex.ru>
2023-03-28 15:29:06 +0500

    without upload scancopy files

--- middleware/validators/docflow.params.validator.js
+++ middleware/validators/docflow.params.validator.js
@@ -1,4 +1,6 @@
+const fs = require('fs/promises');
 const { isValidObjectId } = require('mongoose');
+const logger = require('../../libs/logger');
 
 module.exports.title = async (ctx, next) => {
   if (!_checkTitle(ctx.request?.body?.title)) {
@@ -10,7 +12,7 @@ module.exports.title = async (ctx, next) => {
 
 module.exports.objectId = async (ctx, next) => {
   if (!_checkObjectId(ctx.params.id)) {
-    ctx.throw(400, 'invalid action id');
+    ctx.throw(400, 'invalid doc id');
   }
 
   await next();
@@ -32,10 +34,28 @@ module.exports.taskId = async (ctx, next) => {
   await next();
 };
 
+module.exports.scanCopy = async (ctx, next) => {
+  _deleteFile(ctx.request.files);
+
+  await next();
+};
+
 function _checkObjectId(id) {
   return isValidObjectId(id);
 }
 
 function _checkTitle(title) {
   return title?.trim();
+}
+
+function _deleteFile(files) {
+  for (const file of Object.values(files)) {
+    // received more than 1 file in any field with the same name
+    if (Array.isArray(file)) {
+      _deleteFile(file);
+    } else {
+      fs.unlink(file.filepath)
+        .catch((error) => logger.error(error.mesasge));
+    }
+  }
 }
--- routes/doc.flow.routes.js
+++ routes/doc.flow.routes.js
@@ -1,9 +1,32 @@
+const { readdir, mkdir } = require('node:fs/promises');
 const Router = require('koa-router');
 const { koaBody } = require('koa-body');
 
 const controller = require('../controllers/docflow.controller');
 const validator = require('../middleware/validators/docflow.params.validator');
 const accessCheck = require('../middleware/access.check');
+
+(async () => {
+  try {
+    await readdir('./files/photo');
+  } catch (error) {
+    mkdir('./files/photo', {
+      recursive: true,
+    });
+  }
+})();
+
+const optional = {
+  formidable: {
+    uploadDir: './files',
+    allowEmptyFiles: false,
+    minFileSize: 1,
+    multiples: true,
+    hashAlgorithm: 'md5',
+    keepExtensions: true,
+  },
+  multipart: true,
+};
 
 const router = new Router({ prefix: '/api/informator/docflow' });
 
@@ -12,19 +35,21 @@ router.get('/:id', validator.objectId, controller.get);
 router.get('/', controller.getAll);
 router.post(
   '/',
-  koaBody({ multipart: true }),
+  koaBody(optional),
   validator.title,
   validator.directingId,
   validator.taskId,
+  validator.scanCopy,
   controller.add,
 );
 router.patch(
   '/:id',
-  koaBody({ multipart: true }),
+  koaBody(optional),
   validator.objectId,
   validator.title,
   validator.directingId,
   validator.taskId,
+  validator.scanCopy,
   controller.update,
 );
 router.delete('/:id', validator.objectId, controller.delete);
